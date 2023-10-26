061f928e257b735f9bcff3e5f93fbcd4fa3aa7c6
AduragbemiShobowale <shobowaleaduragbemi24@gmail.com>
2023-10-26 15:07:43 +0100

    typo

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -4,8 +4,8 @@ import { Link } from "react-router-dom";
 import { AiFillEyeInvisible, AiFillEye } from "react-icons/ai";
 import ShowEye from "../../components/ShowEye/ShowEye";
 import { useForm } from "react-hook-form";
-import Spinner from "../../utils/spinner";
 import useAuth from "../../hooks/useAuth";
+import Spinner from "../../utils/spinner";
 
 const SignUp = () => {
   const [passwordVisible, setPasswordVisible] = useState(false);
@@ -18,7 +18,7 @@ const SignUp = () => {
     formState: { errors },
   } = useForm();
 
-  const btnText = authenticating ? <Spinner /> : "Login to your account";
+  const btnText = authenticating ? <Spinner/>: "Login to your account";
 
   const onSubmit = (data) => {
     handleRegisterUser(data);
